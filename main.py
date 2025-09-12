581e59406ed58e30fb1940d20c7edae33aea45f9
jmario18 <135153025+jmario18@users.noreply.github.com>
2025-09-11 22:55:35 -0300

    Ajustes em organização e estrutura principal
    Organização das estruturas principais das páginas,

--- main.py
+++ main.py
@@ -1,28 +1,50 @@
 from model.Paciente import Paciente
 from model.Profissional import Profissional
 from connection.connection import *
+from flask import request, render_template, Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template('index.html')
+
+@app.route("/cadastrar_paciente", methods=["GET", "POST"])
+def cadastrarPacienteRoute():
+    if request.method == "POST":
+        cadastrarPaciente()
+    return render_template('cadastro.html', tipo='paciente')
+
+
+@app.route("/cadastrar_profissional", methods=["GET","POST"])
+def cadastrarProfissionalRoute():
+    if request.method == "POST":
+        cadastrarProfissional()
+    return render_template('cadastro.html', tipo='profissional')
+
 
 def cadastrarPaciente():
-    #pega os dados do paciente
-    nome = input("Digite o nome do paciente: ")
-    idade = input("Digite a idade do paciente: ")
-    endereco = input("Digite o endereço do paciente: ")
-    telefone = input("Digite o telefone do paciente: ")
+    #pega os dados do paciente do formulário na route  
+    nome = request.form.get("nome")
+    dataNasc = request.form.get("data_nascimento")
+    endereco = request.form.get("endereco")
+    telefone = request.form.get("telefone")
     #cria o objeto paciente
-    paciente = Paciente(nome, idade, endereco, telefone)
+    paciente = Paciente(nome, dataNasc, endereco, telefone)
+    print(f"Paciente: {paciente.nome}, Data de Nascimento: {paciente.dataNasc}, Endereço: {paciente.endereco}, Telefone: {paciente.telefone}")
     #cadastra o paciente no banco de dados
-    try:
-        registrarPaciente(paciente.nome, paciente.idade, paciente.endereco, paciente.telefone)
-        print(f"Paciente {paciente.nome} cadastrado com sucesso!")
-    except Exception as e:
-        print(f"Erro ao cadastrar paciente: {e}")
+   # try:
+   #     registrarPaciente(paciente.nome, paciente.dataNasc, paciente.endereco, paciente.telefone)
+   #     print(f"Paciente {paciente.nome} cadastrado com sucesso!")
+   # except Exception as e:
+   #     print(f"Erro ao cadastrar paciente: {e}")
 
 def cadastrarProfissional():
-    nome = input("Digite o nome do Profissional")
-    email = input("Digite o email do Profissional")
-    telefone = input("Digite o telefone do Profissional")
-    cargo = input("Digite o cargo do Profissional")
-    crm = input("Digite o CRM do Profissional")
+    nome = request.form.get("nome")
+    email = request.form.get("email")
+    telefone = request.form.get("telefone")
+    cargo = request.form.get("cargo")
+    crm = request.form.get("crm")
     profissional = Profissional(nome, email, telefone, cargo, crm)
     try:
         registrarProfissional(profissional.nome, profissional.email, profissional.telefone, profissional.cargo, profissional.crm)
@@ -40,9 +62,9 @@ def cadastrarConsulta():
     pass
 
 def cadastrarLocal():
-    nome = input("Digite o nome da unidade: ")
-    endereco = input("Digite o endereço da unidade: ")
-    tipo = input("Digite o tipo da unidade: ")
+    nome = request.form.get("nome")
+    endereco = request.form.get("endereco")
+    tipo = request.form.get("tipo")
     try:
         registrarLocal(nome, endereco, tipo)
         print(f"Unidade {nome} cadastrada com sucesso!")
@@ -53,6 +75,7 @@ def cadastrarReceita():
     pass
 
 def cadastrarProntuario():
+    
     # TODO Disponível para profissionais.
     # TODO Cadastrar prontuário para paciente que seja a primeira consulta
     # TODO Pegar informações da consulta
@@ -61,14 +84,14 @@ def cadastrarProntuario():
     pass
 
 def verificarPaciente():
-    nome = input("Digite o nome do paciente: ")
+    nome = request.form.get("nome")
     consultaPaciente(nome)
 
 def verificarProfissional():
-    nome = input("Digite o nome do profissional: ")
+    nome = request.form.get("nome")
     consultaProfissional(nome)
 
 
 def verificarProntuario():
-    nome = input("Digite o nome do paciente: ")
+    nome = request.form.get("nome")
 #    consultaProntuario(nome)
